92e8606624ce60f9bc9cec2b878f6bc54f8d93ee
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2021-12-29 22:27:53 +0100

    Bumb version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = open("README.md").read()
 
 setup(
     name="aiohwenergy",
-    version="0.5.1",
+    version="0.5.2",
     license="Apache License 2.0",
     url="https://github.com/DCSBL/aiohwenergy",
     author="DCSBL",
